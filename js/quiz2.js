ee95b48ab91506b489bb04e9931b28071d43d26e
Jamie Alden <jamielynn_21@Macintosh.local>
2016-09-06 21:11:06 -0600

    Near final styles

--- js/quiz2.js
+++ js/quiz2.js
@@ -1,72 +1,72 @@
 var questions = [
 	{
-		question:"When Fry discovers that he's a billionaire, what does he buy?",
-		choices:["A Lucy Liu robot", "A lifetime supply of Slurm", "The last tin of sardines on Earth", "The Planet Express"],
+		question:"When Fry discovers that he's a billionaire, what is the most extravagant thing he buys?",
+		choices:["A Lucy Liu robot", "A lifetime supply of Slurm", "The last tin of anchovies on Earth", "The Planet Express"],
 		correctIndex:"2",
-		answer: "The last tin of sardines on Earth"
+		answer: "He bought the last tin of anchovies on Earth, for 50 million dollars."
 	},
 
 	{
 		question:"Finish this phrase: 'Morbo is pleased, but ____'",
 		choices:["Angry", "Sticky", "Smelly", "Confused"],
 		correctIndex:"1",
-		answer:"Sticky"
+		answer:"Morbo is pleased, but STICKY!"
 	},
 
 	{
 		question:"What is the only creature on Earth that Bender is sympathetic to?",
 		choices:["Turtles", "Goats", "Cats", "Butterflies"],
 		correctIndex:"0",
-		answer:"Turtles"
+		answer:"Bender loves turtles, as they both have difficulty getting off their backs."
 	},
 
 	{
 		question:"What is missing from Zapp Brannigan's uniform?",
 		choices:["Boots", "Gloves", "Pants", "Belt"],
 		correctIndex:"2",
-		answer:"Pants"
+		answer:"Pants, much to everyone's dismay."
 	},
 
 	{
 		question:"How is Professor Farnsworth related to Fry?",
 		choices:["Great (x30) grandson", "Great (x30) nephew", "2nd cousin 20 times removed", "He isn't"],
 		correctIndex:"1",
-		answer:"Great (x30) nephew"
+		answer:"Farnsworth is Fry's great (x30) nephew."
 	},
 
 	{
 		question:"Who is Bender's evil twin?",
 		choices:["Calculon", "Roberto", "Flexo", "Coilette"],
 		correctIndex:"2",
-		answer:"Flexo"
+		answer:"Flexo. The only physical difference between the two is a small triangular metal goatee on Flexo's chin."
 	},
 
 	{
 		question:"What is Zoidberg's first name?",
 		choices:["John", "Phillip", "Kevin", "James"],
 		correctIndex:"0",
-		answer:"John"
+		answer:"John. His full name is Doctor John A. Zoidberg."
 	},
 
 	{
-		question:"How does the Femputer sentence Fry, Zapp, and Kif to die?",
+		question:"How does the Femputer sentence Fry, Zapp, and Kif to die on the planet Amazonia?",
 		choices:["By firing squad", "By snu snu", "By dinosaur attack", "By suicide booth"],
 		correctIndex:"1",
-		answer:"By snu snu"
+		answer:"By snu snu. The spirit is willing, but the flesh is spongy and bruised."
 	},
 
 	{
 		question:"What sport does Hermes compete in during the 2980 Olympics?",
 		choices:["Octopus Wrestling", "Blernsball", "Swurling", "Limbo"],
 		correctIndex:"3",
-		answer:"Limbo"
+		answer:"Limbo. Unfortunately, he loses to Barbados Slim."
 	},	
 
 	{
 		question:"What is Zoidberg a doctor of?",
 		choices:["Art History", "General Medicine", "Podiatry", "Physics"],
 		correctIndex:"0",
-		answer:"Art History"
+		answer:"Zoidberg's official doctorate is in Art History, though he acts as staff medical doctor for the Planet Express."
 	}
 ]
 
@@ -94,10 +94,10 @@ var quiz = {
 		if (userAnswer===correctAnswer) {
 			quiz.score++;
 			$("#score").text(quiz.score);
-			$("#result").text("Correct!");
+			$("#result").text("Correct!").css("color", "#139510");
 		}
 		else {
-			$("#result").text("Wrong")
+			$("#result").text("Wrong").css("color", "#e13222");
 		}
 	},
 
